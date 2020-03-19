86bec8a39ed330d4c8b65bc674d6e0a7d17e3277
Gerrit <gerrit.sevilla@gmail.com>
2020-03-19 20:30:04 +0100

    deployer

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -8,7 +8,7 @@ var filesToCache = [
     '/images/icons/icon-72x72.png',
     '/images/icons/icon-96x96.png',
     '/images/icons/icon-144x144.png',
-    '/images/icons/icon-192x192.png'
+    '/images/icons/icon-192x192.png']
 ;
 
 // Cache on install
