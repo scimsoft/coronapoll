f6a3848a7c5d232684fddf675a892a63d44186cf
Gerrit <gerrit.sevilla@gmail.com>
2020-03-19 20:43:06 +0100

    deployer

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -11,14 +11,13 @@ return [
         'display' => 'standalone',
         'orientation'=> 'any',
         'icons' => [
-            '72x72' => '/images/icons/icon-72x72.png',
-            '96x96' => '/images/icons/icon-96x96.png',
-            '128x128' => '/images/icons/icon-128x128.png',
+
+            '36x36' => '/images/icons/icon-36x36.png',
+            '48x48' =>'/images/icons/icon-48x48.png',
+            '72x72' =>'/images/icons/icon-72x72.png',
+            '96x96' =>'/images/icons/icon-96x96.png',
             '144x144' => '/images/icons/icon-144x144.png',
-            '152x152' => '/images/icons/icon-152x152.png',
-            '192x192' => '/images/icons/icon-192x192.png',
-            '384x384' => '/images/icons/icon-384x384.png',
-            '512x512' => '/images/icons/icon-512x512.png'
+            '192x192' =>'/images/icons/icon-192x192.png'
         ],
         'splash' => [
             '640x1136' => '/images/icons/splash-640x1136.png',
