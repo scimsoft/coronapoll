f8598f82cf80b74cab794dcea20c2753f5b6b503
Gerrit <gerrit.sevilla@gmail.com>
2020-03-22 01:15:58 +0100

    mejoras welcome view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,56 +31,60 @@
 
 
                 <div class="card">
-                    <div class="card-header font-weight-bold"> @lang('diagnosis.Temperatura'):</div>
+                    <div class="card-header font-weight-bold" data-toggle="collapse" data-target="#tempraturecard"> @lang('diagnosis.Temperatura'):</div>
+                    <div class="collapse show" id="tempraturecard">
                     <div class="btn-group" data-toggle="buttons">
                         <img src="images/termo.png" style="margin-right: 5px">
-                        <label class="btn btn-success center form-check-label ">
+                        <label class="btn btn-success center form-check-label " data-toggle="collapse" data-target="#tempraturecard">
                             <input class="form-check-input" type="radio" name="options" id="option1" autocomplete="off">
                             @lang('diagnosis.normaltemp')
                         </label>
-                        <label class="btn btn-warning center form-check-label">
+                        <label class="btn btn-warning center form-check-label" data-toggle="collapse" data-target="#tempraturecard">
                             <input class="form-check-input" type="radio" name="options" id="option2"
                                    autocomplete="off"> @lang('diagnosis.fever')
                         </label>
-                        <label class="btn btn-danger center form-check-label">
+                        <label class="btn btn-danger center form-check-label" data-toggle="collapse" data-target="#tempraturecard">
                             <input class="form-check-input" type="radio" name="options" id="option3"
                                    autocomplete="off"> @lang('diagnosis.highfever')
                         </label>
                     </div>
                 </div>
-
+                </div>
                 <div class="card">
-                    <div class="card-header font-weight-bold"> @lang('diagnosis.Cough'):</div>
+                    <div class="card-header font-weight-bold" data-toggle="collapse" data-target="#coughcard"> @lang('diagnosis.Cough'):</div>
+                    <div class="collapse show" id="coughcard">
                     <div class="btn-group" data-toggle="buttons">
                         <img src="images/cough.png" style="margin-right: 5px">
-                        <label class="btn btn-success center form-check-label ">
+                        <label class="btn btn-success center form-check-label " data-toggle="collapse" data-target="#coughcard">
                             <input class="form-check-input" type="radio" name="options" id="option1" autocomplete="off">
                             @lang('diagnosis.none')
                         </label>
-                        <label class="btn btn-warning center form-check-label">
+                        <label class="btn btn-warning center form-check-label" data-toggle="collapse" data-target="#coughcard">
                             <input class="form-check-input" type="radio" name="options" id="option2"
                                    autocomplete="off"> @lang('diagnosis.alittle')
                         </label>
-                        <label class="btn btn-danger center form-check-label">
+                        <label class="btn btn-danger center form-check-label" data-toggle="collapse" data-target="#coughcard">
                             <input class="form-check-input" type="radio" name="options" id="option3"
                                    autocomplete="off"> @lang('diagnosis.alot')
                         </label>
                     </div>
                 </div>
+                </div>
 
                 <div class="card">
-                    <div class="card-header font-weight-bold"> @lang('diagnosis.Shortness of Breath'):</div>
+                    <div class="card-header font-weight-bold" data-toggle="collapse" data-target="#shortbreathcard"> @lang('diagnosis.Shortness of Breath'):</div>
+                    <div class="collapse show" id="shortbreathcard">
                     <div class="btn-group" data-toggle="buttons">
                         <img src="images/dypnea.png" style="margin-right: 5px">
-                        <label class="btn btn-success center form-check-label ">
+                        <label class="btn btn-success center form-check-label " data-toggle="collapse" data-target="#shortbreathcard">
                             <input class="form-check-input" type="radio" name="options" id="option1" autocomplete="off">
                             @lang('diagnosis.none')
                         </label>
-                        <label class="btn btn-warning center form-check-label">
+                        <label class="btn btn-warning center form-check-label" data-toggle="collapse" data-target="#shortbreathcard">
                             <input class="form-check-input" type="radio" name="options" id="option2"
                                    autocomplete="off"> @lang('diagnosis.alittle')
                         </label>
-                        <label class="btn btn-danger center form-check-label">
+                        <label class="btn btn-danger center form-check-label" data-toggle="collapse" data-target="#shortbreathcard">
                             <input class="form-check-input" type="radio" name="options" id="option3"
                                    autocomplete="off"> @lang('diagnosis.alot')
                         </label>
@@ -88,18 +92,19 @@
                 </div>
 
                 <div class="card">
-                    <div class="card-header font-weight-bold"> @lang('diagnosis.Muscle pain'):</div>
+                    <div class="card-header font-weight-bold" data-toggle="collapse" data-target="#musclepaincard"> @lang('diagnosis.Muscle pain'):</div>
+                    <div class="collapse show" id="musclepaincard">
                     <div class="btn-group" data-toggle="buttons">
                         <img src="images/muscle.png" style="margin-right: 5px">
-                        <label class="btn btn-success center form-check-label ">
+                        <label class="btn btn-success center form-check-label " data-toggle="collapse" data-target="#musclepaincard">
                             <input class="form-check-input" type="radio" name="options" id="option1" autocomplete="off">
                             @lang('diagnosis.none')
                         </label>
-                        <label class="btn btn-warning center form-check-label">
+                        <label class="btn btn-warning center form-check-label" data-toggle="collapse" data-target="#musclepaincard">
                             <input class="form-check-input" type="radio" name="options" id="option2"
                                    autocomplete="off"> @lang('diagnosis.alittle')
                         </label>
-                        <label class="btn btn-danger center form-check-label">
+                        <label class="btn btn-danger center form-check-label" data-toggle="collapse" data-target="#musclepaincard">
                             <input class="form-check-input" type="radio" name="options" id="option3"
                                    autocomplete="off"> @lang('diagnosis.alot')
                         </label>
