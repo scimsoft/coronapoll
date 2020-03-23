38fba9d8b148f992a9d7ce7f55ebbd9e5a25fbe8
Gerrit <gerrit.sevilla@gmail.com>
2020-03-23 01:32:28 +0100

    first map version

--- database/factories/SymptomFactory.php
+++ database/factories/SymptomFactory.php
@@ -9,11 +9,12 @@ $factory->define(Symptom::class, function (Faker $faker) {
     return [
         //
         'ip' => $faker->ipv4,
-        'coX' => $faker->latitude,
-        'coY' => $faker->longitude,
+
+        'latitude' => $faker->latitude(37.365,37.406),
+        'longitude' => $faker->longitude(-6.019,-5.905),
         'location' => rand(1,8),
         'temp' => rand(26,41),
-        'caugh' => rand(1,5),
+        'cough' => rand(1,5),
         'musclePain' => rand(1,5),
         'breathShortness' => rand(1,5),
     ];
