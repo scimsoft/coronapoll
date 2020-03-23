051f6db28bdd7f6fdd31e82680f663d515a45cbb
Gerrit <gerrit.sevilla@gmail.com>
2020-03-23 19:34:18 +0100

    checkin traductions

---
+++ resources/lang/es/views.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    'welcomeheader'=>'STOP COVID-19 ',
+    'welcomename'=>'We can stop the coronavirus together.',
+    'welcometext'=>'If we decide to share information, we can help avoid people in risk to come to areas with a bigger possibility to get contamed. <br> Please register and start sharing your condition. <br> All the information you share will be <bold>anonymous</bold>, and we will not share personal data with third-parties.<br><br> Login and see round you. ',
+
+
+
+    'personal_data_anonymous' => 'Your Anonymousity',
+
+    'personal_data_header' => 'Datos Personales',
+    'personal_data_intro' => 'Por favor permitenos usar sus datos completamente anonimo',
+    'personal_data_warning' => 'Los datos que introduces a continuacion no se comparte con terceros y solo se usa por fines estadisticas.',
+    'personal_data_statitics' => '',
+    'personal_data_location' => 'Necesitamos que das permiso de usar tu localizacion..',
+    'personal_data_buttom' => 'Doy Permiso de usar mi localizacion',
+
+
+    'age' => 'Tu edad',
+    'riskgroup' => 'Eres un persona de riesgo?',
+
+    'make_a_choice' => "Select an option",
+    'lowrisk' => "Poco Riesgo",
+    'mediumrisk' => "Riesgo",
+    'highrisk' => "Mucho Riesgo",
+    'confirmed' => 'Ha salida de una prueba positivo?',
+    'checkindata' => 'Empezar'
+
+];
