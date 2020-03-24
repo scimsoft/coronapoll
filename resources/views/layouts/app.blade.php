7c30bfdf333318fd4888efae2c4fb99e2aeca2b9
Gerrit <gerrit.sevilla@gmail.com>
2020-03-24 19:28:45 +0100

    Language switch

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,11 +34,37 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
+
                     <ul class="navbar-nav mr-auto">
 
                     </ul>
 
                     <!-- Right Side Of Navbar -->
+
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Config::get('languages')[App::getLocale()] }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    @foreach (Config::get('languages') as $lang => $language)
+                                        @if ($lang != App::getLocale())
+                                    <a class="dropdown-item" href="{{ route('lang.switch', $lang) }}">
+                                        {{$language}}
+                                    </a>
+                                        @endif
+                                    @endforeach
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+
+                    </ul>
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
