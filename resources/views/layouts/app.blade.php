6aea21dd218770ada8698c0cea4fe361d395b991
Gerrit <gerrit.sevilla@gmail.com>
2020-03-26 14:31:39 +0100

    twitter tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:type" content="website">
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:creator" content="@gerrit_sevilla">
+    <meta name="twitter:title" content="STOP coronavirus">
+    <meta name="twitter:description" content="A anonymous crowd-data site, with the intention to share our coronavirus symptoms, to plot a map with coronavirus hotspots and how the evoluate. ">
+    <meta name="twitter:image" content="https://0corona.com/images/0coronamap.jpg">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
